56bb68b78f5855c8f0f166cf54b02d0cb1a374dc
Tim Chui <tchui@eos.ubc.ca>
2020-09-17 14:29:40 -0700

    added theme to docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,8 +27,6 @@ author = 'Chris'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.autosectionlabel', \
     'sphinx.ext.napoleon', 'sphinx_automodapi.automodapi', 'recommonmark', \
         'sphinx_markdown_tables', 'sphinxcontrib.inlinesyntaxhighlight', \
@@ -55,7 +53,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
